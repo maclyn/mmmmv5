e1d89967f10692a0a5157c3e26df88492de32313
Maclyn Brandwein <maclyn@maclyn.me>
2024-10-16 00:15:47 -0400

    Initial commit

---
+++ player.gd
@@ -0,0 +1,52 @@
+extends CharacterBody3D
+
+signal look_direction_changed(position: Vector3, rotation: Vector3)
+
+@export var sensitivity = 0.005
+@export var min_angle = -PI / 2
+@export var max_angle = PI / 2
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+var look_rotation: Vector2
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("strafe_left", "strafe_right", "forward", "backwards")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	
+	var angular_velocity = get_platform_angular_velocity()
+	look_rotation.y += angular_velocity.y * delta
+	rotation.y = look_rotation.y
+	
+	look_direction_changed.emit(position, rotation)
+	
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		look_rotation.y -= (event.relative.x * sensitivity)
+		look_rotation.x -= (event.relative.x * sensitivity)
+		look_rotation.x = clamp(look_rotation.x, min_angle, max_angle)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
