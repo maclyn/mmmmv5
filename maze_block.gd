4f1c88bed84533afc9da3e908543b96fab148556
Maclyn Brandwein <maclyn@maclyn.me>
2024-10-26 23:57:25 -0400

    Crunchier look

--- maze_block.gd
+++ maze_block.gd
@@ -5,14 +5,19 @@ func _init():
 	pass
 
 func _ready():
-	$HedgeWallN.rotate_z(PI if randi_range(0, 1) == 0 else 0)
-	$HedgeWallE.rotate_z(PI if randi_range(0, 1) == 0 else 0)
-	$HedgeWallS.rotate_z(PI if randi_range(0, 1) == 0 else 0)
-	$HedgeWallW.rotate_z(PI if randi_range(0, 1) == 0 else 0)
-	$HedgeCornerNE.rotate_z(PI if randi_range(0, 1) == 0 else 0)
-	$HedgeCornerSE.rotate_z(PI if randi_range(0, 1) == 0 else 0)
-	$HedgeCornerSW.rotate_z(PI if randi_range(0, 1) == 0 else 0)
-	$HedgeCornerNW.rotate_z(PI if randi_range(0, 1) == 0 else 0)
+	$HedgeWallN.rotate_z(PI if randi_range(0, 1) == 0 else 0.0)
+	$HedgeWallE.rotate_z(PI if randi_range(0, 1) == 0 else 0.0)
+	$HedgeWallS.rotate_z(PI if randi_range(0, 1) == 0 else 0.0)
+	$HedgeWallW.rotate_z(PI if randi_range(0, 1) == 0 else 0.0)
+	$HedgeCornerNE.rotate_z(PI if randi_range(0, 1) == 0 else 0.0)
+	$HedgeCornerNE.rotate_y((PI / 2) * randi_range(0, 4))
+	$HedgeCornerSE.rotate_z(PI if randi_range(0, 1) == 0 else 0.0)
+	$HedgeCornerSE.rotate_y((PI / 2) * randi_range(0, 4))
+	$HedgeCornerSW.rotate_z(PI if randi_range(0, 1) == 0 else 0.0)
+	$HedgeCornerSW.rotate_y((PI / 2) * randi_range(0, 4))
+	$HedgeCornerNW.rotate_z(PI if randi_range(0, 1) == 0 else 0.0)
+	$HedgeCornerNW.rotate_y((PI / 2) * randi_range(0, 4))
+
 
 func configure_walls(north: bool, east: bool, south: bool, west: bool):
 	#north = false
