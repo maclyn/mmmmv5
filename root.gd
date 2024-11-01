0cd323cf6870351d9f6349c52eac5b6b9f5fe3dc
Maclyn Brandwein <maclyn@maclyn.me>
2024-10-31 22:55:57 -0400

    High scores

--- root.gd
+++ root.gd
@@ -26,6 +26,8 @@ const WEST_SNAKE_EDGE = -SNAKE_LENGTH * 3
 const SOUTH_SNAKE_EDGE = (MAZE_DIMENS_IN_SCENE_SPACE) + (SNAKE_LENGTH * 3)
 const NORTH_SNAKE_EDGE = (-LEAD_IN_DIST * MAZE_BLOCK_SQUARE_SIZE) + (SNAKE_LENGTH * -3)
 
+const SAVE_FILE = "user://score.save"
+
 # All game state
 var blocks: Dictionary = {}
 var snakes: Array[Node] = []
@@ -639,7 +641,40 @@ func _calculate_score() -> int:
 	return floori((pct_better_to_key * 1000) + (pct_better_to_return * 500))
 	
 func _compare_to_last_high_score_and_maybe_update(score: int) -> bool:
+	if !FileAccess.file_exists(SAVE_FILE):
+		_create_new_save(score)
+		return true
+ 
+	var save_file = FileAccess.open(SAVE_FILE, FileAccess.READ_WRITE)
+	var json_string = save_file.get_as_text()
+	var json = JSON.new()
+	var parse_result = json.parse(json_string)
+	if not parse_result == OK:
+		print("Parse error; resetting save: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+		_create_new_save(score)
+		return true
+		
+	var old_score = json.data[str(curr_difficulty)]
+	if old_score > score:
+		# Lower than old high score
+		return false
+		
+	# We did better; update
+	json.data[str(curr_difficulty)] = score
+	var to_str = JSON.stringify(json.data)
+	save_file.resize(0)
+	save_file.store_string(to_str)
 	return true
+	
+func _create_new_save(score: int):
+	var new_save = FileAccess.open(SAVE_FILE, FileAccess.WRITE)
+	var dict = {
+		str(GameDifficulty.EASY): 0,
+		str(GameDifficulty.NORMAL): 0,
+		str(GameDifficulty.SPOOKY): 0
+	}
+	dict[str(curr_difficulty)] = score
+	new_save.store_line(JSON.stringify(dict))
 	
 func _hide_main_menu():
 	$MainMenu.visible = false
