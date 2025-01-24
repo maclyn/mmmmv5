fd2043a4d52024386233959620ea8e2a7b1b8ed7
Maclyn <maclyn@Maclyns-Air.myfiosgateway.com>
2025-01-24 11:43:46 -0500

    Credits

--- main_menu.gd
+++ main_menu.gd
@@ -18,6 +18,21 @@ func _on_normal_button_pressed() -> void:
 
 func _on_button_pressed() -> void:
 	AudioServer.set_bus_mute(0, !AudioServer.is_bus_mute(0))
+	
+func _on_credits_pressed() -> void:
+	const credits_path = "res://misc/CREDITS.txt"
+	var credits_file = FileAccess.open(credits_path, FileAccess.READ)
+	var credits_str = credits_file.get_as_text()
+	var c = AcceptDialog.new()
+	c.title = "Credits"
+	c.dialog_text = credits_str
+	c.ok_button_text = "Got it!"
+	
+	c.get_label().add_theme_font_size_override("font_size", 20)
+	c.get_ok_button().add_theme_font_size_override("font_size", 28)
+	add_child(c)
+	c.popup_centered(Vector2i(300, 200))
+	c.show()
 
 func _on_visibility_changed() -> void:
 	if visible:
