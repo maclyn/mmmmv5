7aa1565d0c18b771e839fb47e546faf1cd501bce
Maclyn Brandwein <maclyn@maclyn.me>
2025-02-04 19:58:57 -0500

    Migrate to v4.4-beta2 + remove Jolt
    This opens the door to web exports, which appear to work with
    experimental threading support enabled (at least in some places).

--- globals.gd
+++ globals.gd
@@ -60,13 +60,13 @@ func set_graphics_mode(mode: String):
 	
 	# Weirdly stuttery without these
 	Engine.physics_jitter_fix = 0
-	if mode != "min":
+	if mode != "min" && OS.get_name() != "Web":
 		Input.set_use_accumulated_input(false)
 	
 	# This makes the game a lot more responsive (at the cost of CPU consumption, but
 	# physics on this game are really easy, so we can eat it in most cases 
 	var refresh = DisplayServer.screen_get_refresh_rate()
-	if mode != "low":
+	if mode != "low" && OS.get_name() != "Web":
 		Engine.physics_ticks_per_second = DisplayServer.screen_get_refresh_rate()
 	
 func get_graphics_mode():
